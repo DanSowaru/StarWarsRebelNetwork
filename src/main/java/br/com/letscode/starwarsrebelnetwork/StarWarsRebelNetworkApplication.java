db0acc260d7c98c02bf1b24f87c8ab82f52f5bf4
Danilo Soares <prof.danilomotasoares@gmail.com>
2022-03-05 15:44:29 -0300

    Hello World! This is the first commit of the project, which was prompted as a Spring project, with Thymeleaf, Spring Dev Tools, Spring Web and Lombok dependencies.

---
+++ src/main/java/br/com/letscode/starwarsrebelnetwork/StarWarsRebelNetworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.starwarsrebelnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsRebelNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsRebelNetworkApplication.class, args);
+    }
+
+}
